11899962d7df3f56ef1c214938a7e9409ca11041
rachit bhutani <rachitbhutani1998@gmail.com>
2017-02-18 10:51:59 +0530

    first commit

---
+++ app/src/main/java/com/example/android/wallpics/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.wallpics;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Uri uri = Uri.parse("android.resource//"+getPackageName()+"/"+R.raw.giphy);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoURI(uri);
+        videoView.start();
+    }
+}
