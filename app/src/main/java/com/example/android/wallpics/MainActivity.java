c0441834f4b4d044a79b60269187384e60cddc3d
rachit bhutani <rachitbhutani1998@gmail.com>
2017-02-19 23:23:19 +0530

    sync of firebase changed, like share set buttons removed

--- app/src/main/java/com/example/android/wallpics/MainActivity.java
+++ app/src/main/java/com/example/android/wallpics/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.wallpics;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,15 +8,32 @@ import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.VideoView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+//        if (mFirebaseUser == null) {
+//            // Not signed in, launch the Sign In activity
+//            startActivity(new Intent(this, SignInActivity.class));
+//            finish();
+//            return;
+//        } else {
+//            mUsername = mFirebaseUser.getDisplayName();
+//            if (mFirebaseUser.getPhotoUrl() != null) {
+//                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+//            }
+//        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<GridItem> imageList= new ArrayList<GridItem>();
