2474a564d8e654d25a3d086c77094303560238e7
rachit bhutani <rachitbhutani1998@gmail.com>
2017-04-03 06:13:14 +0530

    Signing Up with mail complete

--- app/src/main/java/com/example/android/wallpics/SignUpActivity.java
+++ app/src/main/java/com/example/android/wallpics/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -36,35 +37,38 @@ public class SignUpActivity extends AppCompatActivity {
         final EditText newMail = (EditText) findViewById(R.id.new_mail);
         final EditText newPass = (EditText) findViewById(R.id.new_pass);
         final EditText conPass = (EditText) findViewById(R.id.con_pass);
-
-
-
         Button signUp = (Button) findViewById(R.id.sign_up);
+
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signingUp.setMessage("Signing You Up...");
+                signingUp.setMessage(getString(R.string.signing_up_message));
                 signingUp.show();
                 final String name = newName.getText().toString().trim();
                 String mail = newMail.getText().toString().trim();
                 String pass = newPass.getText().toString();
                 String cPass = conPass.getText().toString();
-                Log.i("SignUp", "onClick: "+mail+"  "+pass+"  ");
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    if (Objects.equals(pass, cPass)) {
-                        mAuth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                signingUp.dismiss();
-                                Toast.makeText(SignUpActivity.this, "Welcome To WallPics, " + name + "!", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(SignUpActivity.this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                                finish();
-                            }
-                        });
-                    } else {
-                        signingUp.dismiss();
-                        Toast.makeText(SignUpActivity.this, "Your Passwords Doesn't Match", Toast.LENGTH_SHORT).show();
+                if(!TextUtils.isEmpty(name)&&!TextUtils.isEmpty(mail)&&!TextUtils.isEmpty(pass)&&!TextUtils.isEmpty(cPass)) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        if (Objects.equals(pass, cPass)) {
+                            mAuth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    signingUp.dismiss();
+                                    Toast.makeText(SignUpActivity.this, "Welcome To WallPics, " + name + "!", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SignUpActivity.this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                                    finish();
+                                }
+                            });
+                        } else {
+                            signingUp.dismiss();
+                            Toast.makeText(SignUpActivity.this, R.string.signing_up_mismatch, Toast.LENGTH_SHORT).show();
+                        }
                     }
+                }
+                else {
+                    signingUp.dismiss();
+                    Toast.makeText(SignUpActivity.this, R.string.empty_signing_up, Toast.LENGTH_SHORT).show();
                 }
             }
         });
